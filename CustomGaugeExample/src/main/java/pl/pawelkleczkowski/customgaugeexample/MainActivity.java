46860d982c84b39cfd76b9e4beadbc9512013055
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-07-29 11:43:54 +0200

    Prepare release 1.0.4

--- CustomGaugeExample/src/main/java/pl/pawelkleczkowski/customgaugeexample/MainActivity.java
+++ CustomGaugeExample/src/main/java/pl/pawelkleczkowski/customgaugeexample/MainActivity.java
@@ -1,18 +1,20 @@
 package pl.pawelkleczkowski.customgaugeexample;
 
-import java.util.Locale;
-
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 import pl.pawelkleczkowski.customgauge.CustomGauge;
 import pl.pawelkleczkowskicustomgauge.R;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
 	private CustomGauge gauge2;
 	private CustomGauge gauge3;
 
-	int i;
 	private TextView text1;
 	private TextView text2;
-	
+
+	private Handler handler = new Handler(Looper.getMainLooper());
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,45 +41,46 @@ public class MainActivity extends AppCompatActivity {
 		gauge2 = findViewById(R.id.gauge2);
 		gauge3 = findViewById(R.id.gauge3);
 
-		text1  = findViewById(R.id.textView1);
-		text2  = findViewById(R.id.textView2);
+		text1 = findViewById(R.id.textView1);
+		text2 = findViewById(R.id.textView2);
+
 		text1.setText(String.valueOf(gauge1.getValue()));
-		text2.setText(String.valueOf(gauge2.getValue()));
-    	text2.setText(String.valueOf(gauge2.getValue()));
-    	
-		button.setOnClickListener(new OnClickListener() {
-			
+		text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
+
+		button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				gauge2.setEndValue(800);
 				gauge2.setValue(200);
 				text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
-				new Thread() {
-			        public void run() {
-			        	for (i=0;i<100;i++) {
-					        if (i == 50) {
-						        gauge2.setEndValue(1200);
-					        }
-			                try {
-			                    runOnUiThread(new Runnable() {
-			                        @Override
-			                        public void run() {
-			                        	gauge1.setValue(i*10);
-			                        	gauge2.setValue(200 + i*5);
-			                        	gauge3.setValue(i);
-			                        	text1.setText(String.valueOf(gauge1.getValue()));
-			                        	text2.setText(String.format(Locale.getDefault(), "%1d/%2d", gauge2.getValue(), gauge2.getEndValue()));
-			                        }
-			                    });
-			                    Thread.sleep(50);
-			                } catch (InterruptedException e) {
-			                    e.printStackTrace();
-			                }
-			            }
-			        }
-			    }.start();
+
+				new Thread(() -> {
+					for (int i = 0; i < 100; i++) {
+						int value1 = i * 10;
+						int value2 = 200 + i * 5;
+						int value3 = i;
+
+						if (i == 50) {
+							handler.post(() -> gauge2.setEndValue(1200));
+						}
+
+						handler.post(() -> {
+							gauge1.setValue(value1);
+							gauge2.setValue(value2);
+							gauge3.setValue(value3);
+
+							text1.setText(String.valueOf(value1));
+							text2.setText(String.format(Locale.getDefault(), "%1d/%2d", value2, gauge2.getEndValue()));
+						});
+
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
+				}).start();
 			}
 		});
 	}
-	
 }
